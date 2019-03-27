5c2df7964112676b453a953f2443a9329cdbfa9f
Andrew Yoxsimer <Yox@Andrews-MacBook-Air.local>
2019-03-27 08:11:48 -0700

    start to print results from api call.

--- src/main.js
+++ src/main.js
@@ -4,13 +4,22 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/scss/bootstrap.scss';
 import './sass/styles.scss';
-import {Doc} from 'backend.js';
+import {Doc} from './backend.js';
 
 $("document").ready(function(){
   $("#issueSearch").submit(function(){
+    event.preventDefualt();
     let keyword = $("#symptoms").val();
 
-  })
+    let docbyAil = new Doc();
+    let promise = docbyAil.findAil(keyword);
 
-
+    promise.then(function(response){
+      let body = JSON.parse(response);
+      body.data.forEach(function(element) {
+        let results = (`${element.profile.first_name} ${element.profile.last_name}`);
+        console.log(results);
+      })
+    })
+  });
 })
