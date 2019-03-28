c8127c50ca03f7188d6c070a8dac9d13d3200da0
Student <example@example.com>
2019-03-27 17:18:24 -0700

    add address and name calls from api and set them to a variable.

--- src/backend.js
+++ src/backend.js
@@ -4,7 +4,7 @@ export class Doc {
    return new Promise(function(resolve, reject){
      const request = new XMLHttpRequest();
      const apiKey = process.env.exports.apiKey; //api key variable
-     const url = `https://api.betterdoctor.com/2016-03-01/doctors?&query=${keyword}location=45.5122%2C-122.6587%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${apiKey}`
+     const url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${keyword}&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${apiKey}`
      request.onload = function() {
           if (this.status === 200) {
             resolve(request.response)
--- src/main.js
+++ src/main.js
@@ -7,18 +7,20 @@ import './sass/styles.scss';
 import {Doc} from './backend.js';
 
 $("document").ready(function(){
-  $("#issueSearch").submit(function(){
-    event.preventDefualt();
+  $("#issueSearch").submit(function(event){
+    event.preventDefault();
     let keyword = $("#symptoms").val();
 
     let docbyAil = new Doc();
     let promise = docbyAil.findAil(keyword);
 
     promise.then(function(response){
-      let docResponse = JSON.parse(response);
-      docResponse.data.forEach(function(element) {
-        let results = (`${element.profile.first_name} + " " + ${element.profile.last_name}`);
-        console.log(results);
+      let body = JSON.parse(response);
+      body.data.forEach(function(element) {
+        const name = element.profile.first_name + " " + element.profile.last_name;
+        const address = element.practices[0].visit_address.city + " " + element.practices[0].visit_address.state + " " + element.practices[0].visit_address.street;
+        console.log(name);
+        console.log(address);
       })
     })
   });
